1c161cb692361e9c9e909836f33988ec6689fe24
Troy Schrapel <troy@visualrealmsoftware.com>
2020-03-31 10:23:39 +1030

    Hello World example

--- Web/asm/examples/Hello World (LCD).asm
+++ Web/asm/examples/Hello World (LCD).asm
@@ -1,9 +1,11 @@
-lcc #0x38
-lcc #0x0f
+DISPLAY_MODE = LCD_CMD_DISPLAY | LCD_CMD_DISPLAY_ON | LCD_CMD_DISPLAY_CURSOR | LCD_CMD_DISPLAY_CURSOR_BLINK
+
+lcc #LCD_INITIALIZE
+lcc #DISPLAY_MODE
 
 .start:
   clra
-  lcc #1
+  lcc #LCD_CMD_CLEAR
   data Ra, .hello
   call .printStr
   jmz
